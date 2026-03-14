ff3c92475e774b645e372d2245279b7e82961276
luongtiensinh <tien12clash@gmail.com>
2026-03-14 22:15:17 +0700

    fix_error

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,7 +1,9 @@
 import * as userRepository from '../repositories/userRepository.js';
 
 export const getUserById = async (id) => {
-  const user = userRepository.findById(id);
+  const user = await userRepository.findById(id);
 
-  return {id: user.id, username: user.username, role: user.role, fullName: user.full_name} || null;
-}
+  if (!user) return null;
+
+  return { id: user.id, username: user.username, role: user.role, fullName: user.full_name };
+};
