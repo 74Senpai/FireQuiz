21dc5e8db155bc5618a4edcf7fbe71e1caad14ec
74_Senpai <ducthong712004@gmail.com>
2026-04-04 17:08:26 +0700

    add transaction for question

--- backend/src/repositories/questionRepository.js
+++ backend/src/repositories/questionRepository.js
@@ -1,11 +1,11 @@
 import pool from '../db/db.js';
 
-export const create = async (data) => {
+export const create = async (data, tx = pool) => {
   const { content, type, quizId } = data;
  
   console.log(`info: in questionRepository.js:6 content: ${content}, type: ${type}, quizId: ${quizId}`);
   const sql = "INSERT INTO questions(content, type, quiz_id) VALUES (?, ?, ?);";
-  const [row] = await pool.execute(sql, [content, type, quizId]);
+  const [row] = await tx.execute(sql, [content, type, quizId]);
   return row.insertId;
 };
 
@@ -15,16 +15,16 @@ export const findQuestionById = async (id) => {
   return row[0] || null;
 };
 
-export const changeType = async (id, type) => {
+export const changeType = async (id, type, tx = pool) => {
   const sql = "UPDATE questions SET type = COALESCE(?, type) WHERE id = ?;";
 
-  await pool.execute(sql, [type ?? null, id]);
+  await tx.execute(sql, [type ?? null, id]);
 };
 
-export const changeContent = async (id, content) => {
+export const changeContent = async (id, content, tx = pool) => {
   const sql = "UPDATE questions SET content = COALESCE(?, content) WHERE id = ?;";
 
-  await pool.execute(sql, [content ?? null, id]);
+  await tx.execute(sql, [content ?? null, id]);
 };
 
 export const getListQuestionByQuizId = async (id) => {
@@ -34,8 +34,8 @@ export const getListQuestionByQuizId = async (id) => {
   return row;
 };
 
-export const deleteQuestionById = async (id) => {
+export const deleteQuestionById = async (id, tx = pool) => {
   const sql = "DELETE FROM questions WHERE id = ?;";
 
-  await pool.execute(sql, [id]);
+  await tx.execute(sql, [id]);
 };
